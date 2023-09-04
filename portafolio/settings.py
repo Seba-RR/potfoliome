9fd981f4b0f3187ed1f62251c48b7220dd2f43e7
Seba-RR <seba.ramirez.ramos@gmail.com>
2023-09-03 22:24:58 -0300

    test variables

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -146,7 +146,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'seba.ramirez.ramos@gmail.com'
-EMAIL_HOST_PASSWORD = 'elxnguevafpneozm'
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+#EMAIL_HOST_USER = 'seba.ramirez.ramos@gmail.com'
+#EMAIL_HOST_PASSWORD = 'elxnguevafpneozm'
 
 print(os.environ.get('TEST'))
